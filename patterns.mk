2797bc9aaa30c0b49d9604e084498cf342d53d86
David Morse <morse012@umn.edu>
2018-02-20 16:00:57 -0600

    Updating patterns.mk file.

--- patterns.mk
+++ patterns.mk
@@ -10,32 +10,33 @@
 # of main config.mk configuration file of the build directory, and the
 # util/config.mk namespace level config file. 
 #-----------------------------------------------------------------------
+# Makefile variable definitions
+
+# Dependencies on build configuration files
+UTIL_CFGS= -A$(BLD_DIR)/config.mk
+UTIL_CFGS+= -A$(BLD_DIR)/util/config.mk
 
 # All libraries needed in src/util
 LIBS=$(util_LIB)
 
-# Preprocessor macro definitions needed in src/util
-DEFINES=$(UTIL_DEFS)
+#-----------------------------------------------------------------------
+# Pattern rules
 
-# Dependencies on build configuration files
-MAKE_DEPS= -A$(BLD_DIR)/config.mk
-MAKE_DEPS+= -A$(BLD_DIR)/util/config.mk
-
-# Pattern rule to compile *.cpp class source files in src/util
+# Rule to compile *.cpp class source files in src/util
 $(BLD_DIR)/%.o:$(SRC_DIR)/%.cpp
-	$(CXX) $(INCLUDES) $(DEFINES) $(CXXFLAGS) -c -o $@ $<
+	$(CXX) $(INCLUDES) $(UTIL_DEFS) $(CXXFLAGS) -c -o $@ $<
 ifdef MAKEDEP
-	$(MAKEDEP) $(INCLUDES) $(DEFINES) $(MAKE_DEPS) -S$(SRC_DIR) -B$(BLD_DIR) $<
+	$(MAKEDEP) $(INCLUDES) $(UTIL_DEFS) $(CXXFLAGS) $(UTIL_CFGS) -S$(SRC_DIR) -B$(BLD_DIR) $<
 endif
 
-# Pattern rule to compile *.cc test programs in src/util/tests
+# Rule to compile *.cc test programs in src/util/tests
 $(BLD_DIR)/util/tests/%.o: $(SRC_DIR)/util/tests/%.cc 
-	$(CXX) $(INCLUDES) $(DEFINES) $(TESTFLAGS) -c -o $@ $<
+	$(CXX) $(INCLUDES) $(UTIL_DEFS) $(TESTFLAGS) -c -o $@ $<
 ifdef MAKEDEP
-	$(MAKEDEP) $(INCLUDES) $(DEFINES) $(MAKE_DEPS) -S$(SRC_DIR) -B$(BLD_DIR) $<
+	$(MAKEDEP) $(INCLUDES) $(UTIL_DEFS) $(CXXFLAGS) $(UTIL_CFGS) -S$(SRC_DIR) -B$(BLD_DIR) $<
 endif
 
-# Pattern rule to link *.cc test programs in src/util/tests
+# Rule to link *.cc test programs in src/util/tests
 $(BLD_DIR)/util/tests/%: $(BLD_DIR)/util/tests/%.o $(LIBS)
-	$(CXX) $(INCLUDES) $(DEFINES) -o $@ $@.o $(LIBS) $(LDFLAGS)
+	$(CXX) -o $@ $@.o $(LIBS) $(LDFLAGS)
 
