fd73adf5cd688617a9fb37a5e04e3347fa92f81a
David Morse <morse012@umn.edu>
2022-12-11 11:16:16 -0600

    Suppressing compilation of Xdr archive classes.
    This was forced by removal of the SUN RPC library from glibc in
    recent linux releases. The XdrIArchive and XdrOArchive classes
    used the header <rpc/rpc.h> which used to be standard, but is no
    longer available as part of the GNU standard C library, glibc.
    The code for these classes remains, but they will not be compiled
    by default. They should be compilable on a system with an older
    glibc, or a system in which the RPC library has been explicitly
    installed.

--- archives/sources.mk
+++ archives/sources.mk
@@ -6,9 +6,12 @@ util_archives_=\
     util/archives/BinaryFileOArchive.cpp \
     util/archives/BinaryFileIArchive.cpp \
     util/archives/TextFileOArchive.cpp \
-    util/archives/TextFileIArchive.cpp \
-    util/archives/XdrFileOArchive.cpp \
-    util/archives/XdrFileIArchive.cpp 
+    util/archives/TextFileIArchive.cpp 
+
+ifdef UTIL_XDR
+util_archives_ += util/archives/XdrFileIArchive.cpp \
+                  util/archives/XdrFileOArchive.cpp 
+endif
 
 util_archives_SRCS=$(addprefix $(SRC_DIR)/, $(util_archives_))
 util_archives_OBJS=$(addprefix $(BLD_DIR)/, $(util_archives_:.cpp=.o))
